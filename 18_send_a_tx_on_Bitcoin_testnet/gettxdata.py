bb0ab06122979a1c119244278bd900d37fcf0431
pttismylp <99028049+pttismylp@users.noreply.github.com>
2023-08-04 04:47:31 +0800

    Delete 18_send_a_tx_on_Bitcoin_testnet directory

--- 18_send_a_tx_on_Bitcoin_testnet/gettxdata.py
+++
@@ -1,9 +0,0 @@
-from requests_html import HTMLSession
-from lxml import html
-etree = html.etree
-session = HTMLSession()
-r = session.get('https://api.blockcypher.com/v1/btc/test3/addrs/mkZwNtbAYtp4zimgvm8ayvHD5dU3bkTYNd')
-with open("txdata.txt","w",encoding='utf-8') as f:
-    f.write(r.html.full_text)
-
-
